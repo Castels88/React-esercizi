99eff139a2a607724a1e3348c36bf7bb81371047
[elio] <[castels@hotmail.it]>
2022-08-29 08:53:52 +0100

    esercizio 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { HelloWorld } from "./Helloworld";
+import { Name } from "./Name";
 export class App extends React.Component{
 render(){
     return (
     <div>
         <HelloWorld/>
+        <h1>Hello <Name/></h1>
     </div>
     )
 }
---
+++ src/Name.js
@@ -0,0 +1,6 @@
+import React from "react";
+export class Name extends React.Component{
+    render(){
+        return <span>Elio</span>
+    }
+}
