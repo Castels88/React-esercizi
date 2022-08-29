f2f483e1a56d5eb1673f0d07282e8e6f92af6371
[elio] <[castels@hotmail.it]>
2022-08-29 11:50:47 +0100

    esercizio 7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HelloWorld } from "./Helloworld";
 import { Message } from "./Message";
 import { Name } from "./Name";
+import { Welcome } from "./Welcome";
 export class App extends React.Component{
 render(){
     return (
@@ -9,6 +10,7 @@ render(){
         <HelloWorld/>
         <h1>Hello <Name/></h1>
         <h2>{sum(2,3)}</h2>
+        <Welcome name="Elio"/>  
     </div>
     )
 }
