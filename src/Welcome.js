38e24255c76b3e53571df5e11a7b16d51a8e52ed
[elio] <[castels@hotmail.it]>
2022-08-29 11:55:59 +0100

    esercizio 8

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,6 +1,17 @@
 import React from "react"
 export class Welcome extends React.Component{
     render(){
-        return <p> Hello , {this.props.name}</p>
+        return(
+            <div>
+                {
+                    this.props.name
+                    ? <p>Ciao, {this.props.name}</p>
+                    : <p>Ciao, Sconosciuto</p>
+                }
+            </div>
+        ) 
     }
 }
+
+// in questo caso utilizzando elementi condizionali posso decidere di settare una opzione 
+// qualora non venga stabilito un nome
