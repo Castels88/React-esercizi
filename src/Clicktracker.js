8d2e44ec4f4db463c3e6af6f52d317851e797da5
[elio] <[castels@hotmail.it]>
2022-09-05 12:34:09 +0100

    event 04

--- src/Clicktracker.js
+++ src/Clicktracker.js
@@ -1,4 +1,7 @@
 import React from "react";
+import arrow_up from "./img/arrow_up.png"
+import arrow_right from "./img/arrow_r.png"
+import arrow_left from "./img/arrow_l.png"
 export class ClickTracker extends React.Component{
     state = {
         TrackEvent: ""
@@ -18,9 +21,15 @@ export class ClickTracker extends React.Component{
     render(){
         return(
             <div>
-                <button name="PrimoBTN" onClick={this.handleButton}>Primo</button>
-                <button name="SecondoBTN" onClick={this.handleButton}>Secondo</button>
-                <button name="TerzoBTN" onClick={this.handleButton}>Terzo</button>
+                <button name="PrimoBTN" onClick={this.handleButton}>
+                <img src={arrow_up} alt="freccia su" />
+                </button>
+                <button name="SecondoBTN" onClick={this.handleButton}>
+                <img src={arrow_right} alt="" />
+                </button>
+                <button name="TerzoBTN" onClick={this.handleButton}>
+                <img src={arrow_left} alt="" />
+                </button>
                 <h1>{this.state.TrackEvent}</h1>
             </div>
         )
@@ -32,3 +41,7 @@ export class ClickTracker extends React.Component{
 // Implement a single event handler for all three buttons, and track of which button was pressed last. 
 // Show the last button that was pressed in an h1 tag within the ClickTracker component. 
 // Tip: you can use the event object to access the target property of the event.
+
+// Events - 04
+// Modifiy the ClickTracker so that each button contains an image instead of a text.
+//  How does this affect the event handler?
