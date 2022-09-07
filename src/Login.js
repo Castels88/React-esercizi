d8d62e45dc95bce75c1a78c05b62e6fe1c3a9f54
[elio] <[castels@hotmail.it]>
2022-09-07 11:30:12 +0100

    form-04 fine esercizio

--- src/Login.js
+++ src/Login.js
@@ -37,8 +37,5 @@ export class Login extends React.Component{
     }
 }
 
-// Forms - 03
-// Add a "login" button to the Login component. 
-// This button should be disabled as long as the username and password inputs are empty. 
-// When clicked, the event handler attached to the button should call an 
-// onLogin function passed as a prop to the Login component, passing it the state.
+// Forms - 04
+// Add a "reset" button to the Login component that clears the content of all three inputs when clicked.
