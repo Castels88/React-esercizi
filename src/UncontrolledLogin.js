63dc169a4654b0aa31cb5010d68c3bbd266e7317
[elio] <[castels@hotmail.it]>
2022-09-07 14:59:24 +0100

    form-05 fine esercizio

--- src/UncontrolledLogin.js
+++ src/UncontrolledLogin.js
@@ -5,23 +5,19 @@ export class UncontrolledLogin extends React.Component{
         const username = event.target.elements.username.value
         const password = event.target.elements.password.value
         const remember = event.target.elements.remember.checked
-
-        console.log(
-            {
-                username,
-                password,
-                remember,
-            }
-        )
+        console.log({
+            username,
+            password,
+            remember,
+        })
     }
     render(){
         return (
             <div>
-                <h3>Uncontrolled Login</h3>
                 <form onSubmit={this.handleFormLogin}>
                     <input type="text" name="username" />
-                    <input type="password" name="password" />
-                    <input type="checkbox" name="remember" />
+                    <input type="text" name="password" />
+                    <input type="text" name="remember" />
                     <button type="submit">Login</button>
                     <button type="reset">Reset</button>
                 </form>
