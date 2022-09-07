2df32262b4b876cb04c77acbd4fb0f505a386512
[elio] <[castels@hotmail.it]>
2022-09-07 11:19:33 +0100

    form-03 fine esercizio

--- src/Login.js
+++ src/Login.js
@@ -14,18 +14,23 @@ export class Login extends React.Component{
             [name] : type === "checkbox" ? checked : value 
         })
     }
+    Onlogin = ()=>{
+        console.log(this.state)
+    }
     render(){
         return(
             <div>
                 <input type="text" name="username" value={this.state.username} onChange={this.HandleInputChange} />
                 <input type="password" name="password" value={this.state.password} onChange={this.HandleInputChange}/>
                 <input type="checkbox" name="remember" checked={this.state.remember} onChange={this.HandleInputChange}/>
+                <button disabled={!this.state.username||!this.state.password} onClick={this.Onlogin}>Login</button>
             </div>
         )
     }
 }
 
-// Forms - 02
-// Create a Login component containing three 
-// inputs: a username input, a password input and a remember checkbox. 
-// All three inputs should be controlled components.
+// Forms - 03
+// Add a "login" button to the Login component. 
+// This button should be disabled as long as the username and password inputs are empty. 
+// When clicked, the event handler attached to the button should call an 
+// onLogin function passed as a prop to the Login component, passing it the state.
