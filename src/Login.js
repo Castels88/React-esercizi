2280b25c8440121c4191b95d88e2160c94c1ebfc
[elio] <[castels@hotmail.it]>
2022-09-11 12:51:59 +0100

    finer esercizio

--- src/Login.js
+++ src/Login.js
@@ -25,18 +25,23 @@ export class Login extends React.Component{
         })
     }
     render(){
+        const myStyle = {
+            backgroundColor:this.state.password.length <= 8 ? "red":"green",
+            width:"95px",
+        }
         return(
             <div className="flex flex-col">
                 <h3 className="my-3">Controlled component</h3>
                 <input className="border border-black, w-64" type="text" name="username" value={this.state.username} onChange={this.HandleInputChange} />
                 <input className="border border-black, w-64" type="password" name="password" value={this.state.password} onChange={this.HandleInputChange}/>
                 <input className="border border-black, w-64" type="checkbox" name="remember" checked={this.state.remember} onChange={this.HandleInputChange}/>
-                <button className="border border-black, p-1, bg-sky-500 hover:bg-sky-700, w-24 " disabled={!this.state.username||!this.state.password} onClick={this.Onlogin}>Login</button>
+                <button style={myStyle} disabled={!this.state.username||!this.state.password} onClick={this.Onlogin}>Login</button>
                 <button className="border border-black, p-1, bg-sky-500 hover:bg-sky-700, w-24" onClick={this.ResetState} >Reset</button>
             </div>
         )
     }
 }
 
-// Forms - 04
-// Add a "reset" button to the Login component that clears the content of all three inputs when clicked.
+// Styling Components - 03
+// Modify the Login component from Forms 03 so that the "login" button background color is 
+// "red" when the inputted password's length is shorter than 8 characters, green otherwise.
