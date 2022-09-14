b3121ff594ee01fce52cc74561107811c3f689de
[elio] <[castels@hotmail.it]>
2022-09-14 16:39:13 +0100

    fine esercizio

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,27 +1,17 @@
-import React from "react";
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-export class ClickCounter extends React.Component{
-    state = {
-        count : this.props.InitialValue
-    }
-    
-    IncrementValue = (event) => {
-        console.log(event)
-        this.setState((state)=>{
-            return{
-                count: state.count + 1
-            }
-        })
-    }
 
-    render(){
-        return(
-            <Card border="primary" style={{width: "15rem"}}> 
-                <h2>Click Counter</h2>
-                <h3>{this.state.count}</h3>
-                <Button variant="success" style={{width: "15rem"}} onClick={this.IncrementValue}>Incrementa</Button>{''}
-            </Card>
-        )
+export function ClickCounter({inizialValue = 0}){
+    const [counter, setCounter] = useState(inizialValue)
+    function IncrementValue(){
+        setCounter(c => c + 1 )
     }
+    return(
+        <Card border="primary" style={{width: "15rem"}}> 
+            <h2>Click Counter</h2>
+            <h3>{counter}</h3>
+            <Button variant="success" style={{width: "15rem"}} onClick={IncrementValue}>Incrementa</Button>{''}
+        </Card>
+    )
 }
