ed598f46051816ec32ab82dcb9582559bedbaecf
[elio] <[castels@hotmail.it]>
2022-09-17 13:11:52 +0100

    fine esercizio

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { Fragment } from "react";
-import { ClickCounter } from "./ClickCounter";
 import { Container } from "./Container";
-import { DisplayLanguage } from "./DisplayLanguage";
-import { LanguageContext } from "./LanguageContext ";
 import { Counter } from "./Counter"
-import { GithubUser } from "./GithubUser";
+import { GithubUser } from "./GithubUser"
 
 export function App (){
 
     return (
     <Container title={"La mia App"}>
         <Counter/>
-        <GithubUser username="Castels88" />
+        <GithubUser username="Castels88"/>
     </Container>
     )
 }
--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,20 +1,40 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
-export function GithubUser({username}){
-    const [data, setData] = useState(null)
-    useEffect(()=>{
-        fetch(`https://api.github.com/users/${username}`)
-        .then(response=>{
-            return response.json()
-        })
-        .then(json=>{
-            console.log(json)
-            setData(json)
-        })
-    })
-    return(
-        <div>
-            {data.name}
-        </div>
-    )
+export function GithubUser({ username }) {
+const [user, setUser] = useState(null);
+const [loading, setLoading] = useState(false);
+const [error, setError] = useState(null);
+
+async function fetchGithubUser(username) {
+    setLoading(true);
+    setError(null);
+
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        const json = await response.json();
+        setUser(json);
+    } catch (error) {
+        setError(error);
+    } finally {
+        setLoading(false);
+    }
+}
+
+    useEffect(() => {
+    fetchGithubUser(username);
+}, [username]);
+
+    return (
+    <div>
+        {loading && <p>Loading...</p>}
+        {error && <p>Error</p>}
+        {user && 
+    <div>
+        <h1>{`Name: ${user.name}`}</h1>
+        <h1>{`URL: ${user.url}`}</h1>
+    </div>
+
+    }
+    </div>
+  )
 }
