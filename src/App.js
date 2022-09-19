69265c7c67e70913ea51a0aafa6d6280210acf08
[elio] <[castels@hotmail.it]>
2022-09-19 22:38:08 +0100

    fine esercizio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Fragment } from "react";
 import { ClickCounter } from "./ClickCounter"
+import { DisplayLanguage } from "./DisplayLanguage";
+import { useState } from "react";
+import { LanguageContext } from "./LanguageContext "
 export function App (){
-
+    const [language, setLanguage] = useState("en")
+    function handleChangeLanguage (event){
+        setLanguage(event.target.value)
+    }
     return (
         <div>
-            <ClickCounter/>
+            <select value={language} onChange={handleChangeLanguage}>
+                    <option value="en">English</option>
+                    <option value="it">Italian</option>
+                </select>
+                <LanguageContext.Provider value={language}>
+                    <DisplayLanguage/>
+                </LanguageContext.Provider>
         </div>
     )
 }
