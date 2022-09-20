4d39697eefde44e752ddc9f3ca856e029574b981
[elio] <[castels@hotmail.it]>
2022-09-20 18:25:42 +0100

    fine esercizio

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Container} from "./Container"
 import {Welcome} from "./Welcome"
 import {ClickCounter} from "./ClickCounter"
 import { Counter } from "./Counter";
+import { GithubUser } from "./GithubUser";
 export function App (){
     return (
         <Container title={
@@ -14,12 +15,12 @@ export function App (){
             <Routes>
                 <Route path="/" element={<Welcome/>}/>
                 <Route path="/counter" element={<Counter/>}/>
+                <Route path="/:username" element={<GithubUser username="Castels88"/>}/>
             </Routes>
             
         </Container>
     )
 }
-// React Router - 02
-// Add a new Route to the /counter path that 
-// renders the Counter component from useState 01.
-
+// React Router - 03
+// Add a new Route to the users/:username path that renders a ShowGithubUser component that receives the username as a path parameter and renders the GithubUser 
+// component from useEffect 03 by passing it the received username.
