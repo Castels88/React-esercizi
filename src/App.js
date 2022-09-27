bf3d06649e75673bbcc21470cb8293a30b858afc
[elio] <[castels@hotmail.it]>
2022-09-27 17:43:04 +0100

    fine esercizio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from "react";
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Link} from "react-router-dom"
 import {Container} from "./Container"
 import {Welcome} from "./Welcome"
-import {ClickCounter} from "./ClickCounter"
+import {Message} from "./Message"
 import { Counter } from "./Counter";
 import { GithubUser } from "./GithubUser";
+import { Login } from "./Login"
 export function App (){
     return (
         <Container title={
             <div>
                 <h1>My App </h1>
+                <div>
+                    <Link to="/counter">Counter</Link> | <Link to="/login">Login</Link> | 
+                    <Link to="/message">Message</Link>
+                </div>
             </div>
         }>
             <Routes>
                 <Route path="/" element={<Welcome/>}/>
                 <Route path="/counter" element={<Counter/>}/>
                 <Route path="/:username" element={<GithubUser username="Castels88"/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/message" element={<Message/>}/>
             </Routes>
             
         </Container>
     )
 }
-// React Router - 03
-// Add a new Route to the users/:username path that renders a ShowGithubUser component that receives the username as a path parameter and renders the GithubUser 
-// component from useEffect 03 by passing it the received username.
+// React Router - 04
+// Add three Links within the main App component and use them to navigate to all three routes.
